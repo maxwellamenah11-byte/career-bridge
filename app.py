de714ac7a08f6c31d85739ae1add5f0de62aef9b
maxwellamenah11-byte <maxwellamenah11@gmail.com>
2026-08-27 15:15:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ def about():
     return render_template("about.html")
 
 
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
 @app.route("/services")
 def services():
     return render_template("services.html")
