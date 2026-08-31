939f3ec31144bfafe9b127426bcaa8ad9c79326f
maxwellamenah11-byte <maxwellamenah11@gmail.com>
2026-08-31 16:27:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,16 @@ app.config["SECRET_KEY"] = os.environ.get(
     "career-bridge-development-key"
 )
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///career_bridge.db"
+database_url = os.environ.get("DATABASE_URL")
+
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
