eccdd7fd1c105661ed0ba76323e59429b4dbea61
maxwellamenah11-byte <maxwellamenah11@gmail.com>
2026-09-10 22:28:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,7 @@
-from flask import (
-    Flask,
-    render_template,
-    request,
-    redirect,
-    url_for,
-    session,
-    jsonify,
-    send_from_directory
-)
-
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import (
-    generate_password_hash,
-    check_password_hash,
-    secure_filename
-)
-
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
 from functools import wraps
 from datetime import datetime
 from sqlalchemy import inspect, text
